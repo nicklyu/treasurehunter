c4433aa1a49b53deed1b4cfe1c863442eb1dd9ff
lyubimov nick <lyubimov.nkt@gmail.com>
2017-12-09 22:32:39 +0300

    Fixed [rest] TRGAME #7 Методы API 'Get all levels'

--- src/main/java/ru/game/http/RestApiRouter.java
+++ src/main/java/ru/game/http/RestApiRouter.java
@@ -15,46 +15,49 @@ public class RestApiRouter {
     private DatabaseService dbService;
 
 
-    public void init(Vertx vertx, Router router){
+    public void init(Vertx vertx, Router router) {
 
         dbService = DatabaseService.createProxy(vertx, DB_QUEUE);
 
         router.get("/api/levels").handler(this::getAllLevels);
-        router.get("/api/:levelid/info").handler(this::getLevelInfo);
-        router.get(PREFIX+"area").handler(this::getLevelArea);
-        router.get(PREFIX+"tip").handler(this::getTips);
-        router.get(PREFIX+"tip/:tipId").handler(this::getTipByNumber);
-        router.get(PREFIX+"initialtip").handler(this::getInitialTip);
-        router.get(PREFIX+"treasure").handler(this::getTreasure);
+        router.get(PREFIX + "info").handler(this::getLevelInfo);
+        router.get(PREFIX + "area").handler(this::getLevelArea);
+        router.get(PREFIX + "tip").handler(this::getTips);
+        router.get(PREFIX + "tip/:tipId").handler(this::getTipByNumber);
+        router.get(PREFIX + "initialtip").handler(this::getInitialTip);
+        router.get(PREFIX + "treasure").handler(this::getTreasure);
 
     }
 
     /**
      * Возвращает список из всех уровней
+     *
      * @param context
      */
-    private void getAllLevels(RoutingContext context){
-        dbService.fetchAllLevels(reply->{
-           if(reply.succeeded()){
-               JsonArray levels = reply.result();
-               context.response().end(levels.toString());
-           } else {
-               context.fail(reply.cause());
-           }
+    private void getAllLevels(RoutingContext context) {
+        dbService.fetchAllLevels(reply -> {
+            if (reply.succeeded()) {
+                JsonArray levels = reply.result();
+                context.response().end(levels.toString());
+            } else {
+                context.fail(reply.cause());
+            }
         });
     }
+
     /**
      * Возвращает полную информацию об уровне
      * - Имя и описание
      * - Расположение арены
      * - Расположение подсказок
      * - Расположение сокровища
+     *
      * @param context
      */
-    private void getLevelInfo(RoutingContext context){
+    private void getLevelInfo(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
-        dbService.fetchLevelInfo(levelId, reply->{
-            if(reply.succeeded()){
+        dbService.fetchLevelInfo(levelId, reply -> {
+            if (reply.succeeded()) {
                 JsonObject level = reply.result();
                 context.response().end(level.toString());
             } else {
@@ -65,28 +68,30 @@ public class RestApiRouter {
 
     /**
      * Возвращает расположение арены уровня
+     *
      * @param context
      */
-    private void getLevelArea(RoutingContext context){
+    private void getLevelArea(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
-        dbService.fetchLevelArea(levelId, reply->{
-           if(reply.succeeded()){
-               JsonObject area = reply.result();
-               context.response().end(area.toString());
-           } else {
-               context.fail(reply.cause());
-           }
+        dbService.fetchLevelArea(levelId, reply -> {
+            if (reply.succeeded()) {
+                JsonObject area = reply.result();
+                context.response().end(area.toString());
+            } else {
+                context.fail(reply.cause());
+            }
         });
     }
 
     /**
      * Возвращает расположение всех подсказок для уровня
+     *
      * @param context
      */
-    private void getTips(RoutingContext context){
+    private void getTips(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
-        dbService.fetchAllTips(levelId, reply->{
-            if(reply.succeeded()){
+        dbService.fetchAllTips(levelId, reply -> {
+            if (reply.succeeded()) {
                 JsonArray tips = reply.result();
                 context.response().end(tips.toString());
             } else {
@@ -97,14 +102,15 @@ public class RestApiRouter {
 
     /**
      * Возвращает расположение первой подсказки
+     *
      * @param context
      */
-    private void getInitialTip(RoutingContext context){
+    private void getInitialTip(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
-        dbService.fetchAllTips(levelId, reply->{
-            if(reply.succeeded()){
+        dbService.fetchAllTips(levelId, reply -> {
+            if (reply.succeeded()) {
                 JsonArray tips = reply.result();
-                if(tips.size()<1){
+                if (tips.size() < 1) {
                     context.fail(404);
                 } else {
                     context.response().end(tips.getJsonArray(0).toString());
@@ -117,15 +123,16 @@ public class RestApiRouter {
 
     /**
      * Возвращает расположение указанной подсказки
+     *
      * @param context
      */
-    private  void getTipByNumber(RoutingContext context){
+    private void getTipByNumber(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
         Integer tipNum = Integer.parseInt(context.pathParam("tipId"));
-        dbService.fetchAllTips(levelId, reply->{
-            if(reply.succeeded()){
+        dbService.fetchAllTips(levelId, reply -> {
+            if (reply.succeeded()) {
                 JsonArray tips = reply.result();
-                if(tips.size()<tipNum){
+                if (tips.size() < tipNum) {
                     context.fail(404);
                 } else {
                     context.response().end(tips.getJsonArray(tipNum).toString());
@@ -138,12 +145,13 @@ public class RestApiRouter {
 
     /**
      * Возвращает расположение сокровища
+     *
      * @param context
      */
-    private void getTreasure(RoutingContext context){
+    private void getTreasure(RoutingContext context) {
         Integer levelId = Integer.parseInt(context.pathParam("levelid"));
-        dbService.fetchLevelTreasure(levelId, reply->{
-            if(reply.succeeded()){
+        dbService.fetchLevelTreasure(levelId, reply -> {
+            if (reply.succeeded()) {
                 JsonObject treasure = reply.result();
                 context.response().end(treasure.toString());
             } else {
