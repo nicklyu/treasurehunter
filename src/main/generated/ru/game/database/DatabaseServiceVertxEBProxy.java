5bf47550113fe340a95b76e3be45f01658dbcb20
lyubimov nick <lyubimov.nkt@gmail.com>
2017-12-09 22:10:30 +0300

    Added [rest] TRGAME #7 Методы API 'Tips controller'

--- src/main/generated/ru/game/database/DatabaseServiceVertxEBProxy.java
+++ src/main/generated/ru/game/database/DatabaseServiceVertxEBProxy.java
@@ -143,6 +143,25 @@ public class DatabaseServiceVertxEBProxy implements DatabaseService {
     return this;
   }
 
+  public DatabaseService fetchAllTips(int levelId, Handler<AsyncResult<JsonArray>> resultHandler) {
+    if (closed) {
+    resultHandler.handle(Future.failedFuture(new IllegalStateException("Proxy is closed")));
+      return this;
+    }
+    JsonObject _json = new JsonObject();
+    _json.put("levelId", levelId);
+    DeliveryOptions _deliveryOptions = (_options != null) ? new DeliveryOptions(_options) : new DeliveryOptions();
+    _deliveryOptions.addHeader("action", "fetchAllTips");
+    _vertx.eventBus().<JsonArray>send(_address, _json, _deliveryOptions, res -> {
+      if (res.failed()) {
+        resultHandler.handle(Future.failedFuture(res.cause()));
+      } else {
+        resultHandler.handle(Future.succeededFuture(res.result().body()));
+      }
+    });
+    return this;
+  }
+
 
   private List<Character> convertToListChar(JsonArray arr) {
     List<Character> list = new ArrayList<>();
