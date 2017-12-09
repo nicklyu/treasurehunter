99d4dee2f4d871fbe9ebe22205a4b4b20bd3f35e
lyubimov nick <lyubimov.nkt@gmail.com>
2017-12-09 15:05:00 +0300

    Added [rest] TRGAME#1. Структура HttpServerVerticle

--- src/main/java/ru/game/app/MainVerticle.java
+++ src/main/java/ru/game/app/MainVerticle.java
@@ -1,13 +1,11 @@
 package ru.game.app;
 
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
-import ru.game.http.HttpServerVerticle;
+
 
 public class MainVerticle extends AbstractVerticle {
 
